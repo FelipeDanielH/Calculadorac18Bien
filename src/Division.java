ec353510708caf19e2ab98c10994cd5a9da238d7
eriasuru <elias.osorio.jara@gmail.com>
2025-02-20 20:56:04 -0300

    se agrega division

---
+++ src/Division.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Division {
+
+     public void division() {
+
+
+         double dividendo, divisor, resultado;
+
+         Scanner sc = new Scanner(System.in);
+
+         System.out.println("Ingresa el dividendo: ");
+         dividendo = sc.nextDouble();
+
+         System.out.println("Ingresa el divisor: ");
+         divisor = sc.nextDouble();
+         if (divisor == 0) {
+             System.out.println("El divisor no puede ser zero");
+         } else {
+             resultado = dividendo / divisor;
+             System.out.println("El resultado es: " + resultado);
+         }
+         sc.close();
+     }
+
+    }
+
+
+
+
+
