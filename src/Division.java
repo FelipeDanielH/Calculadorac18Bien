3dcfa1afa764cd2eb516579ef71aa272ae5e0d72
Felipe Daniel <felipe.daniel.henriquez@gmail.com>
2025-02-20 21:11:19 -0300

    se une todo el equipo con el poder de la amistad

--- src/Division.java
+++ src/Division.java
@@ -4,7 +4,6 @@ public class Division {
 
      public void division() {
 
-
          double dividendo, divisor, resultado;
 
          Scanner sc = new Scanner(System.in);
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Scanner sc = new Scanner(System.in);
+        int option = 0;
+        //menu
+        System.out.println("menu ingrese numero");
+        System.out.println("1.Division");
+        System.out.println("2.Modulo");
+        System.out.println("3.Raiz Cuadrada");
+        System.out.println("4.Resta");
+        option = sc.nextInt();
+
+
+        if(option == 1){
+            Division division = new Division();
+            division.division();
+        }
+        if(option == 2){
+            Modulo modulo = new Modulo();
+            modulo.Modulo();
+        }
+        if(option == 3){
+            RaizCuadrada raizCuadrada = new RaizCuadrada();
+            raizCuadrada.raizCuadrada();
+        }
+        if(option == 4) {
+            Resta resta = new Resta();
+            resta.resta();
+        }
+
     }
 }
